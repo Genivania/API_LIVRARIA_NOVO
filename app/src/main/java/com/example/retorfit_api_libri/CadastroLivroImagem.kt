56ecb87e563302c4215a1b2683a5915f09724a2d
Genivania Macedo <109962733+Genivania@users.noreply.github.com>
2023-11-14 10:11:00 -0300

    ok

--- app/src/main/java/com/example/retorfit_api_libri/CadastroLivroImagem.kt
+++ app/src/main/java/com/example/retorfit_api_libri/CadastroLivroImagem.kt
@@ -7,7 +7,9 @@ import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 
 class CadastroLivroImagem : AppCompatActivity() {
@@ -36,9 +38,11 @@ class CadastroLivroImagem : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cadastro_livro_imagem)
 
+        initVars()
+
         //TESTE DE RECEBIMENTO DO JSON
-        val bodyJSON = intent.getStringExtra("bodyJSON")
-            Log.e("TESTE-JSON", bodyJSON.toString())
+//        val bodyJSON = intent.getStringExtra("bodyJSON")
+//            Log.e("TESTE-JSON", bodyJSON.toString())
 
         //RECUPERA OS ELEMENTOS DE VIEW DE IMAGENS
         btnImgGRD = findViewById<ImageView>(R.id.imgGRD)
@@ -49,22 +53,74 @@ class CadastroLivroImagem : AppCompatActivity() {
 
         //TRATAMENTO DO EVENTO DE CLICK DO BOTÃO DE IMAGEM GRANDE
         btnImgGRD?.setOnClickListener {
-            Toast.makeText(this,
-                                "BOTÃO DA IMAGEM GRANDE",
-                                    Toast.LENGTH_LONG).show()
+//            Toast.makeText(this,
+//                "BOTÃO DA IMAGEM GRANDE",
+//                 Toast.LENGTH_LONG).show()
+            resultLauncherGRD.launch("image/*")
+
         }
 
         //TRATAMENTO DO EVENTO DE CLICK DO BOTÃO DE IMAGEM PEQUENA
         btnImgPEQ?.setOnClickListener {
-            Toast.makeText(this,
-                "BOTÃO DA IMAGEM PEQUENA",
-                Toast.LENGTH_LONG).show()
+//            Toast.makeText(this,
+//                "BOTÃO DA IMAGEM PEQUENA",
+//                Toast.LENGTH_LONG).show()
+            resultLauncherPEQ.launch("image/*")
         }
         //TRATAMENTO DO EVENTO DE CLICK DO BOTÃO DE CADASTRO
         btnUpload?.setOnClickListener {
-            Toast.makeText(this,
-                "BOTÃO DE CADASTRO",
-                Toast.LENGTH_LONG).show()
+//            Toast.makeText(this,
+//                "BOTÃO DE CADASTRO",
+//                Toast.LENGTH_LONG).show()
+
+        }
+    }
+        //LANÇADOR PARA RECUPERAR IMAGEM GRANDE DA GALERIA PARA O UPLOAD
+        private val resultLauncherGRD =registerForActivityResult(ActivityResultContracts.GetContent()){
+           imageUriGRD = it
+           btnImgGRD?.setImageURI(imageUriGRD)
+          Log.e("IMG-GRD", imageUriGRD.toString())
+        }
+
+        //LANÇADOR PARA RECUPERAR IMAGEM PEQUENA DA GALERIA PARA O UPLOAD
+        private val resultLauncherPEQ =registerForActivityResult(ActivityResultContracts.GetContent()){
+            imageUriPEQ = it
+            btnImgPEQ?.setImageURI(imageUriPEQ)
+            Log.e("IMG-PEQ", imageUriPEQ.toString())
+        }
+
+        //INICIALIZAÇÃO DAS VARIÁVEIS DO FIREBASE
+        private fun initVars() {
+            storageRef = FirebaseStorage.getInstance().reference.child("images")
+            firebaseFirestore = FirebaseFirestore.getInstance()
+        }
+
+
+    //FUNÇÃO DE UPLOAD
+    private fun uploadImage(){
+
+        imageUriGRD?.let {
+            val riversRef = storageRef.child("${it.lastPathSegment}-${System.currentTimeMillis()}.jpg")
+            val uploadTask = riversRef.putFile(it)
+            uploadTask.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    riversRef.downloadUrl.addOnSuccessListener { uri ->
+                        val map = HashMap<String, Any>()
+                        map["pic"] = uri.toString()
+                        firebaseFirestore.collection("images").add(map).addOnCompleteListener { firestoreTask ->
+                            if (firestoreTask.isSuccessful){
+                                Toast.makeText(this, "UPLOAD IMAGEM GRANDE OK!", Toast.LENGTH_SHORT).show()
+                            }else{
+                                Toast.makeText(this, firestoreTask.exception?.message, Toast.LENGTH_SHORT).show()
+                            }
+                            btnImgGRD?.setImageResource(R.drawable.upload)
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+                    btnImgGRD?.setImageResource(R.drawable.upload)
+                }
+            }
         }
 
     }
